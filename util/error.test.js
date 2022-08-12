7cee54907c3ad9e1731dc5559241d9f1b78738da
Nguyen Hoang Gia Bao <giabao@evol.vn>
2022-08-12 10:57:14 +0700

    refactor & optimize testing code for error.js & validation.js

--- util/error.test.js
+++ util/error.test.js
@@ -1,30 +1,76 @@
 import { describe, it, expect } from 'vitest'
-import { HttpError } from './errors'
+import { HttpError, ValidationError } from './errors'
 
-describe('HttpError class', () => {
-    it('should create a new HttpError instance', () => {
-        const inputErr = new HttpError(500, { message: 'Something went wrong!' }, "test")
+const string = 'test';
+const obj = { message: 'Something went wrong!' };
+const num = 500;
+// const emptyString = '';
+// ========== HttpError =================
+describe('HttpError Class', () => {
 
-        expect(inputErr).toBeInstanceOf(HttpError)
+    const error1 = new HttpError()
+    const error2 = new HttpError(num)
+    const error3 = new HttpError(num, obj)
+    const error41 = new HttpError(num, obj, string)
+    const error42 = new HttpError(num, string, string)
+    const error43 = new HttpError(num, obj, obj)
+    const error44 = new HttpError(num, string, obj)
+    const error6 = new HttpError(num, obj, string, string, string)
+    const error5 = new HttpError(num, obj, string, string)
+
+    it('should be an instance if not enough arguments are passed', () => {
+        expect(error1).toBeInstanceOf(HttpError)
+        expect(error2).toBeInstanceOf(HttpError)
+        expect(error3).toBeInstanceOf(HttpError)
     })
 
-    it('should not throw an error if not enough arguments passed', () => {
-        const inputErr1 = new HttpError();
-        const inputErr2 = new HttpError(500)
-        const inputErr3 = new HttpError(500, { message: 'Something went wrong' })
-
-        expect(inputErr1).toBeInstanceOf(HttpError)
-        expect(inputErr2).toBeInstanceOf(HttpError)
-        expect(inputErr3).toBeInstanceOf(HttpError)
-    })
-    it('should not throw an error if more arguments are passed', () => {
-        const inputErr1 = new Error(500, { message: 'test!' }, "test1", "test2")
-        const inputErr2 = new Error(500, { message: 'test!' }, "test1", "test2", "test3")
-
-
-        expect(inputErr1).toBeUndefined()
-        expect(inputErr2).toBeUndefined()
-
+    it('should be an instance if enough arguments are passed', () => {
+        expect(error41).toBeInstanceOf(HttpError)
     })
 
+    it('should be an instance if more arguments are passed', () => {
+        expect(error5).toBeInstanceOf(HttpError)
+        expect(error6).toBeInstanceOf(HttpError)
+    })
+
+    it('has constructor working correctly', () => {
+        expect(error41.statusCode).toEqual(num);
+        expect(error41.message).toEqual(obj);
+        expect(error41.data).toEqual(string);
+
+        expect(error42.statusCode).toEqual(num);
+        expect(error42.message).toEqual(string);
+        expect(error42.data).toEqual(string);
+
+        expect(error43.statusCode).toEqual(num);
+        expect(error43.message).toEqual(obj);
+        expect(error43.data).toEqual(obj);
+
+        expect(error44.statusCode).toEqual(num);
+        expect(error44.message).toEqual(string);
+        expect(error44.data).toEqual(obj);
+    })
 })
+
+// ========== HttpError =================
+describe('ValidationError Class', () => {
+    const valid1 = new ValidationError(num)
+    const valid2 = new ValidationError(string)
+    const valid3 = new ValidationError(obj)
+
+    it('has constructor working correctly', () => {
+        expect(valid1.message).toEqual(num);
+        expect(valid2.message).toEqual(string);
+        expect(valid3.message).toEqual(obj);
+    })
+
+    it('should be an instance, if more or less than one argument is provided', () => {
+        const valid1 = new ValidationError()
+        const valid2 = new ValidationError(string, string)
+        const valid3 = new ValidationError(obj, obj, obj)
+
+        expect(valid1).toBeInstanceOf(ValidationError)
+        expect(valid2).toBeInstanceOf(ValidationError)
+        expect(valid3).toBeInstanceOf(ValidationError)
+    })
+})  
--- util/validation.test.js
+++ util/validation.test.js
@@ -1,11 +1,22 @@
 import { expect, it } from 'vitest';
 import { validateNotEmpty } from './validation'
 
-it('should throw an error if an empty text is provided', () => {
+it('should throw an error with provided message, if an empty text is passed', () => {
     const input = '';
     const testMessage = 'Not an empty string'
 
-    const result = validateNotEmpty(input, testMessage)
+    const resultFn = () => validateNotEmpty(input, testMessage)
 
-    expect(result).toThrow()
+    expect(resultFn).toThrow(testMessage)
+})
+
+it('should not throw an error, if non-empty text is provided', () => {
+    const input1 = 'test message';
+    const input2 = '       test message           ';
+
+    const resultFn1 = () => validateNotEmpty(input1, 'not to throw');
+    const resultFn2 = () => validateNotEmpty(input2, 'not to throw');
+
+    expect(resultFn1).not.toThrow()
+    expect(resultFn2).not.toThrow()
 })
